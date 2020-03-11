ade8937c90d82d8a8e5095e34daf4401be25a734
Alex Newman <thedotmack@gmail.com>
2020-03-11 16:24:56 -0400

    Initial commit

---
+++ wp-content/themes/copter-fitlovesquad/resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html {!! get_language_attributes() !!}>
+  @include('partials.head')
+  <body @php body_class() @endphp>
+    @php do_action('get_header') @endphp
+    @include('partials.header')
+    <div class="wrap container" role="document">
+      <div class="content">
+        <main class="main">
+          @yield('content')
+        </main>
+        @if (App\display_sidebar())
+          <aside class="sidebar">
+            @include('partials.sidebar')
+          </aside>
+        @endif
+      </div>
+    </div>
+    @php do_action('get_footer') @endphp
+    @include('partials.footer')
+    @php wp_footer() @endphp
+
+
+    <script id="__bs_script__">//<![CDATA[
+        document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.26.7'><\/script>".replace("HOST", location.hostname));
+    //]]></script>
+
+  </body>
+</html>
